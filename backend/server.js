86c9ea59d07054b96907e930d26347bf33aa6d77
Deekayyy2420 <hilmanfahmi6@gmail.com>
2026-07-15 10:27:59 +0800

    fix smtp API

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: Number(process.env.EMAIL_PORT) === 465,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
